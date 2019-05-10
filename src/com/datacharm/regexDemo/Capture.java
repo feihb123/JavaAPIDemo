7e4491416aa14d7379dc183fb181503803f9751a
feihb123 <feihb123@vip.qq.com>
2019-05-11 00:10:14 +0800

    demo

--- src/com/datacharm/regexDemo/Capture.java
+++ src/com/datacharm/regexDemo/Capture.java
@@ -5,11 +5,21 @@ import java.util.regex.Pattern;
 
 public class Capture {
     public static void main(String[] args) {
-        String pro = "[\"1\"]";
-        System.out.println(pro);
-        String pattern = "\\*?(\\d+)";
+
+        String line = "[\"1234\"]";
+        System.out.println(line);
+        String pattern = "(\\D*)?(\\d+)(.*)";
+        // 创建 Pattern 对象
         Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(pro);
-        System.out.println(m.group(0));
+        // 创建 matcher 对象
+        Matcher m = r.matcher(line);
+        if (m.find( )) {
+            System.out.println("Found value: " + m.group(0) );
+            System.out.println("Found value: " + m.group(1) );
+            System.out.println("Found value: " + m.group(2) );
+            System.out.println("Found value: " + m.group(3) );
+        } else {
+            System.out.println("NO MATCH");
+        }
     }
 }
